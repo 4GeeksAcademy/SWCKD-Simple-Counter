8fa006ade8281f3ff07bba3e0b354904e2c4a817
Alejandro <aleckbeu@gmail.com>
2024-04-01 18:35:37 +0000

    Alejandro

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,12 +8,12 @@ const Home = (props) => {
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{height: "300px"}}><i class="fas fa-clock"></i></div>
-				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{height: "300px"}}>{props.digitFour % 10}</div>
-				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{height: "300px"}}>{props.digitThree % 10}</div>
-				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{height: "300px"}}>{props.digitTwo % 10}</div>
-				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{height: "300px"}}>{props.digitOne % 10}</div>
-				
+				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{ height: "300px" }}><i class="fas fa-clock"></i></div>
+				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{ height: "300px" }}>{props.digitFour % 10}</div>
+				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{ height: "300px" }}>{props.digitThree % 10}</div>
+				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{ height: "300px" }}>{props.digitTwo % 10}</div>
+				<div className="shadow-sm col border bg-light border rounded mx-1 d-flex flex-column justify-content-center align-items-center text-center" style={{ height: "300px" }}>{props.digitOne % 10}</div>
+
 			</div>
 		</div>
 	);
